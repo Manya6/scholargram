6ccee00541a8097cf03fd32d8d53071c0de93525
Manya Malhotra <malhotra.manyaaaa@gmail.com>
2024-07-30 11:37:50 +0100

    Initial Commit

---
+++ scholargram/scholargramApp.swift
@@ -0,0 +1,17 @@
+//
+//  scholargramApp.swift
+//  scholargram
+//
+//  Created by Manya Malhotra on 30/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct scholargramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
