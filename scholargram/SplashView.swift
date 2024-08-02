221e403518208a861982b240033de7e309ea512d
Manya Malhotra <malhotra.manyaaaa@gmail.com>
2024-08-02 11:07:33 +0100

    idk

---
+++ scholargram/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  scholargram
+//
+//  Created by Manya Malhotra on 31/07/2024.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
